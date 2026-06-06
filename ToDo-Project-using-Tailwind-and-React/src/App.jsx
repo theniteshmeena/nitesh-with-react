ed3c5a7324176f68b10511d2b86d632f2d56f902
theniteshmeena <theniteshmeena@gmail.com>
2026-06-06 14:07:05 +0530

    fix: update dev script in package.json and restore App.css import

--- ToDo-Project-using-Tailwind-and-React/src/App.jsx
+++ ToDo-Project-using-Tailwind-and-React/src/App.jsx
@@ -2,10 +2,10 @@ import { useEffect, useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "./assets/vite.svg";
 import heroImg from "./assets/hero.png";
-import "./App.css";
 import { TodoProvider } from "./contexts";
 import TodoForm from "./components/TodoForm";
 import TodoItem from "./components/TodoItem";
+import "./App.css";
 
 function App() {
   const [todos, setTodos] = useState([]);
